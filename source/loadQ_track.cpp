c4ab6c410e4bc6e1d0d0ea0cc40d358e81bc3f05
kilean20 <kilean20@gmail.com>
2016-03-05 18:32:13 -0500

    [Lin] amend

--- source/loadQ_track.cpp
+++ source/loadQ_track.cpp
@@ -151,7 +151,7 @@ int main(int argc, char *argv[])
     unsigned int *lost_turn = new unsigned int[N_particle];
     unsigned int *lost_post = new unsigned int[N_particle];
     double phix,phiz,Ax,Az;
-    for (unsigned int i=0; i < N_particle-100; i++) {
+    for (unsigned int i=200; i < N_particle; i++) {
         phix=unifRand(-M_PI,M_PI);
         Ax=sqrt(betax0*epsx)*trunc_xgaussRand(4);
         x[i*8+0] = Ax*cos(phix);
@@ -163,11 +163,12 @@ int main(int argc, char *argv[])
         x[i*8+4] = 0;
         x[i*8+5] = 0;
     }
-    int dummyAx=1, dummyPhi=1;
-    for (unsigned int i=100; i < N_particle; i++) {
-        if(dummyPhi==4){dummyAx++; dummyPhi=-5;}
-        phix=M_PI*(double)dymmplyPhi/10.0;
-        Ax=sqrt(betax0*epsx)*0.5*dummyAx;
+    int dummyAx=1;
+    double dummyPhi=-0.5;
+    for (unsigned int i=0; i < 200; i++) {
+        if(dummyPhi>0.4){dummyAx++; dummyPhi=-0.5; cout<< "i=" << i << ",  phi=" << dummyPhi <<  endl;}
+        phix=M_PI*0.1*dummyPhi;
+        Ax=sqrt(betax0*epsx)*0.3*dummyAx;
         x[i*8+0] = Ax*cos(phix);
         x[i*8+6] = Ax*sin(phix); //Px
         phiz=unifRand(-M_PI,M_PI);
@@ -176,7 +177,7 @@ int main(int argc, char *argv[])
         x[i*8+7] = Az*sin(phiz); //Pz
         x[i*8+4] = 0;
         x[i*8+5] = 0;
-        dummyPhi++
+        dummyPhi+=0.05;
     }
     for (unsigned int i=0; i < N_particle; i++) {
         x[i*8+1] = (x[i*8+6]-alfax0*x[i*8+0])/betax0;
